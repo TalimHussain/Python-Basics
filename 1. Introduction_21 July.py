8ccdfc642259f06c3348e152c3b3e28a5eb73ac5
TalimHussain <92736379+TalimHussain@users.noreply.github.com>
2021-11-21 02:16:37 +0530

    Add files via upload

---
+++ 1. Introduction_21 July.py
@@ -0,0 +1,74 @@
+"Introduction to Python"
+
+"""
+Instructions for the class
+1. Always keep a notebook with you and take notes
+2. During the class, no one should have their laptops / systems with them. Note down the code
+in your notebook only with proper syntax
+
+
+Q. What is python?
+A. Python is a simple and high level programming language
+
+Q. What is the use of any programming language?
+A. Any programming language is used to create softwares
+
+Q. What kind of softwares can we create using python?
+A. We can create two types of softwares:
+    1. Character User Interface (CUI) Applications : These are the softwares in which the 
+    commands are provided by typing them through the keyboard.
+    Examples are: Command Prompt in windows
+    
+    
+    2. Graphical User Interface (GUI) Applications : 
+    a grapical user inyterface (gui) allows a user to intract with computer program using a pointing device that 
+    manipulates small piactures on a computer screen .
+    
+
+DJango or Jango
+
+
+5 steps to create variables:
+
+Step 1:
+Amount of memory required to save the values is determined
+
+Step 2:
+The program searches for the required amount of memory in RAM
+
+Step 3:
+The memory location is reserved
+
+Step 4:
+Values are saved in the memory location
+
+Step 5:
+Variables are assigned to the memory locations
+
+
+Types of variables:
+
+1. Value type variables
+2. Reference type variables
+
+
+Types of memory allocations:
+
+1. Static Memory Allocation
+2. Dynamic Memory Allocation
+
+
+Types of Programming Languages:
+
+1. Compiler Based
+2. Interpreter Based
+"""
+
+
+
+
+
+
+
+
+

f26d645f1067d421589e842454c8cdd440aa09b6
TalimHussain <92736379+TalimHussain@users.noreply.github.com>
2021-11-21 02:31:33 +0530

    Add files via upload

---
+++ 14. List_25 Aug.py
@@ -0,0 +1,140 @@
+"""List in Python"""
+
+"""
+Lists in python are a data type which store multiple heterogeneous data types
+
+How are lists created?
+Using squared brackets []
+"""
+
+# l = [1, 2, 3, 4]
+
+# # New Program
+# l = [5, 87.65, "Hello"]
+# print(l)
+# l.append(10)
+# print(l)
+
+
+# # New Program
+# l = [5, 87.65, "Hello"]
+# print(l)
+# l.insert(1, 10)
+# print(l)
+
+# # New Program
+# l = [5, 87.65, "Hello"]
+# print(l)
+# res = l.index(10)
+# print(res)
+
+# # New Program
+# l = [5, 87.65, "Hello", 5, 5, 5, 5, 5]
+# print(l)
+# res = l.count(5)
+# print(res)
+
+
+# # New Program
+# l = [5, 87.65, "Hello", 5, 54, 52156.412, "Welcome", 5]
+# print(l)
+# res = l.pop(5)
+# print(l)
+
+
+"""
+The pop method removes the values from the list based on index positions
+By default it removes the value at last index, but we can provide it index numbers 
+manually also
+"""
+
+# # New Program
+# l = [5, 87.65, "Hello", 5, 54, 52156.412, "Welcome", 5]
+# print(l)
+# res = l.remove(5)
+# print(l)
+
+
+"""
+The remove method deletes elements from the list based on values 
+"""
+
+# # New Program
+# l = [5, 87.65, "Hello", 5, 54, 52156.412, "Welcome", 5]
+# print(l)
+# res = l.reverse()
+# print(l)
+
+# # New Program
+# l = [5, 87.65, "Hello", 5, 54, 52156.412, "Welcome", 5]
+# print(l)
+# res = l.extend("Python")
+# print(l)
+
+# # New Program
+# l = [5, 8, 32, 1.6, 54, 2.5, 3, 7.9, 5, 4]
+# print(l)
+# res = l.sort()
+# print(l)
+
+# # New Program
+# l = [5, 8, 32, 1.6, 54, 2.5, 3, 7.9, 5, 4]
+# print(l)
+# res = l.sort(reverse = True)
+# print(l)
+
+
+# # New Program
+# l = [5, 8, 32, 1.6, 54, 2.5, 3, 7.9, 5, 4]
+# print(l)
+# res = l.clear()
+# print(l)
+
+
+# # New Program
+# l = [5, 8, 32, 1.6, 54, 2.5, 3, 7.9, 5, 4]
+# l1 = l
+# print(l1)
+# print(l)
+#
+# l1[3] = 150
+# print(l1)
+# print(l)
+
+
+# # New Program
+# l = [5, 8, 32, 1.6, 54, 2.5, 3, 7.9, 5, 4]
+# print(l)
+# l1 = l.copy()
+# print(l1)
+#
+# l1[3] = 150
+# print(l1)
+# print(l)
+
+
+"""
+Next topic will be database connectivity:
+
+library: pymysql
+
+
+Apart from this, 'MySQL Database Server' should be installed on your system
+
+How to install it?
+
+root123
+"""
+
+
+
+
+
+
+
+
+
+
+
+
+
